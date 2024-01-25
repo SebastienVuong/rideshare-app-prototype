efba4be929d1c3e7d12615b6f9790c1f948d2f33
Sébastien Vuong <sebastien.vuong.teamcanada@gmail.com>
2024-01-25 16:21:27 -0500

    setup: add absolute import paths, sort imports
    Fixing import order to avoid big git diff's based solely on import changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,32 @@
 module.exports = {
   root: true,
   extends: "@react-native",
+  plugins: ["import"],
   rules: {
     quotes: ["error", "double"],
     semi: ["error", "never"],
+    "import/order": [
+      "error",
+      {
+        groups: ["external", "internal", ["sibling", "parent"]],
+        pathGroups: [
+          {
+            pattern: "react**",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "app/**",
+            group: "internal",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["internal"],
+        alphabetize: {
+          order: "asc",
+        },
+        "newlines-between": "always",
+      },
+    ],
   },
 }
